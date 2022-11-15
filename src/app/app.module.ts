c2afcaa000e13bcb60318d0d820ca000540ddb5a
JuanYanqui <100297911+JuanYanqui@users.noreply.github.com>
2022-11-15 17:04:37 -0500

    cambio inicio
    cambio incioo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
